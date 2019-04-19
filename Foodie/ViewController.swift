6c91481646baf37359c929bb47fd25f8313a7573
Haiying Weng <haiyingweng@gmail.com>
2019-04-18 22:42:27 -0400

    Initial Commit

---
+++ Foodie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foodie
+//
+//  Created by HAIYING WENG on 4/18/19.
+//  Copyright © 2019 Hack Challenge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
